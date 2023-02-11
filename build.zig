05b68e65ed5fb2adeb88349ea0ec4a5c1546b0e5
hjalmare <maileye@gmail.com>
2023-02-11 16:10:57 +0100

    addtesttobuild

--- build.zig
+++ build.zig
@@ -1,10 +1,15 @@
 const Builder = @import("std").build.Builder;
 
-
 pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
-    
+
+    const main_tests = b.addTest("src/main.zig");
+    main_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
+
     const exe = b.addExecutable("dstr", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
