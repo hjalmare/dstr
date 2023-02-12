c59581c91d69924de9b6b41c9b89d87840e02f04
hjalmare <maileye@gmail.com>
2023-02-12 15:34:53 +0100

    add release stuff

--- build_pak.sh
+++ build_pak.sh
@@ -3,6 +3,6 @@
 mkdir -p releases
 rm -rf zig-out
 zig build -Dtarget=$1
-tar -cvzf releases/$1.tar.gz -C zig-out/bin/ dstr
+tar -cvzf releases/dstr-$1.tar.gz -C zig-out/bin/ dstr
 
 
---
+++ rel_tag.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export BRANCH_A=main
+export BRANCH_B=origin/main
+
+
+if [ x"$(git rev-parse $BRANCH_A)" != x"$(git rev-parse $BRANCH_B)" ]
+then
+    echo $BRANCH_A and $BRANCH_B are not the same
+    exit 1
+fi
+
+export TAG = $(date -I)-$(expr $(git tag | wc -l) + 1)
+git tag $TAG
+git push origin $TAG
